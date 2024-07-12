a64828077bcacd2d8c79dff709cc8c292794163f
Juan T. Palacios <juante92@gmail.com>
2024-07-12 00:15:27 -0400

    added assistants 2 and 3

--- action/src/assisstants.d.ts
+++ action/src/assisstants.d.ts
@@ -1,5 +1,7 @@
 declare const assistants: {
     'weekly-assessment-1': string;
+    'weekly-assessment-2': string;
+    'weekly-assessment-3': string;
     theory: string;
 };
 export default assistants;
--- src/assisstants.ts
+++ src/assisstants.ts
@@ -1,5 +1,7 @@
 const assistants = {
   'weekly-assessment-1': 'asst_Kbd0h4si27PFBnq3SGj7zZiZ',
+  'weekly-assessment-2': 'asst_KJ1JX9R1M0Vd7ZxtWLdTjPSc',
+  'weekly-assessment-3': 'asst_ebrHvr56yloZdau39voRVg2R',
   theory: 'asst_hUipAqnIgZUsgYv7o4Ck4RYC',
 }
 
